6facc65dde8c094cd3b5a06832174c18b364e847
Dwaipayan Sardar <dwaipayan.sardar@vitra.ai>
2023-06-24 12:54:40 +0530

    UI Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,6 @@ export default function RootLayout({
           async
           src={`https://code.responsivevoice.org/responsivevoice.js?key=${process.env.NEXT_PUBLIC_RESPONSIVE_VOICE_API_KEY}`}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-     (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "hos6ggstt8");`,
-          }}
-        />
-        ;
       </head>
       <body className={inter.className}>{children}</body>
     </html>
